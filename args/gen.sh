479f8047d9e2dc80e6951719e9f8344d31bc8694
Olivier Poitrey <rs@rhapsodyk.net>
2018-09-13 02:45:03 -0700

    Update curl options

--- args/gen.sh
+++ args/gen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -h|perl -pe 's/^\s*(?:-([^-]),\s+)?--(.*?)\s+<.*?>.*/push @a, $1; push @b, $2/e; undef $_; END {print "package args\n\nvar (\n\tcurlShortValues = \"", @a, "\"\n\tcurlLongValues  = []string{", join(", ", map {"\"$_\""} @b), "}\n)\n"}' > curlopts.go
+curl -h|perl -pe 's/^\s*(?:-([^-]),\s+)?--(.*?)\s+<.*?>.*/push @a, $1; push @b, $2/e; undef $_; END {print "package args\n\nvar (\n\tcurlShortValues = \"", @a, "\"\n\tcurlLongValues  = []string{\n", join(",\n", map {"\t\t\"$_\""} @b), "}\n)\n"}' > curlopts.go
