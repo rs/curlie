c589190e9a4a00f6dd74b7facd34abc770f45962
Stefan Everts <js-everts@users.noreply.github.com>
2023-07-03 17:43:52 +0530

    Revert "fix headers/body order (#55)" (#74)
    This reverts commit e63e657919499bc0a826c5fce82ce451ba98ea8f.
    
    The commit above introduced sane-ordering to the terminal output by
    prioritizing headers first and then the response body. But unfortunately
    it introduces a deadlock in cases where we have no headers to print.
    
    Commands like `curlie` or `curlie -h` will hang.
    
    Sometimes it also causes the program to deadlock even when headers are
    present. It probably has something to do with the code failing to
    properly detect where the headers' section ends. But this is difficult
    to reproduce.

--- main.go
+++ main.go
@@ -138,28 +138,14 @@ func main() {
 		fmt.Println()
 		return
 	}
-
 	cmd := exec.Command("curl", opts...)
 	cmd.Stdin = stdin
 	cmd.Stdout = stdout
-
-	tmpOut := &formatter.HeaderCleaner{
+	cmd.Stderr = &formatter.HeaderCleaner{
 		Out:     stderr,
 		Verbose: verbose,
 		Post:    input,
 	}
-
-	if terminal.IsTerminal(stdoutFd) && terminal.IsTerminal(stderrFd) && !quiet {
-		headerBlock := make(chan struct{})
-		cmd.Stdout = &blockedWrite{
-			ch:  headerBlock,
-			out: stdout,
-		}
-		tmpOut.HeadersDone = headerBlock
-	}
-
-	cmd.Stderr = tmpOut
-
 	if (opts.Has("I") || opts.Has("head")) && terminal.IsTerminal(stdoutFd) {
 		cmd.Stdout = ioutil.Discard
 	}
@@ -188,14 +174,4 @@ func headerSupplied(opts args.Opts, header string) bool {
 		}
 	}
 	return false
-}
-
-type blockedWrite struct {
-	ch  <-chan struct{}
-	out io.Writer
-}
-
-func (c *blockedWrite) Write(p []byte) (n int, err error) {
-	<-c.ch
-	return c.out.Write(p)
 }
